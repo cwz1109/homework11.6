5a0e058bd5f75de3a07d28d73bc7b1cec9b9a114
cwz <chenweize1109@163.com>
2023-11-06 19:22:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def quadraticFormula1(a,b,c):
+  y=(-b+(b**2-4*a*c)**0.5)/(2*a)
+  return(y)
+print(quadraticFormula1(1,4,3))
+
+def quadraticFormula2(a,b,c):
+  y=(-b-(b**2-4*a*c)**0.5)/(2*a)
+  return(y)
+print(quadraticFormula2(2,8,6))
